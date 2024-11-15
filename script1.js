89e840d65444c8b2db614a03f042ac204618f5fb
Allmite <nurshat260@gmail.com>
2024-11-15 23:15:20 +0500

    add project

--- script1.js
+++ script1.js
@@ -10,3 +10,15 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
         window.location.href = 'teacher.html';
     }
 });
+    const button = document.getElementById('navigate-btn');
+    const clickSound = document.getElementById('click-sound');
+
+    button.addEventListener('click', () => {
+        // Проигрывание звука
+        clickSound.play();
+
+        // Задержка перед переходом, чтобы звук успел проиграться
+        setTimeout(() => {
+            window.location.href = 'https://example.com'; // URL целевой страницы
+        }, 500); // Установите задержку в зависимости от длины звука (в миллисекундах)
+    });
--- script2.js
+++ script2.js
@@ -42,3 +42,16 @@ document.getElementById('teacherProfileForm').addEventListener('submit', functio
         .then(() => alert('Профиль учителя сохранен!'))
         .catch(error => console.error("Ошибка при сохранении профиля: ", error));
 });
+    const button = document.getElementById('navigate-btn');
+    const clickSound = document.getElementById('click-sound');
+
+    button.addEventListener('click', () => {
+        // Проигрывание звука
+        clickSound.play();
+
+        // Задержка перед переходом, чтобы звук успел проиграться
+        setTimeout(() => {
+            window.location.href = 'https://example.com'; // URL целевой страницы
+        }, 500); // Установите задержку в зависимости от длины звука (в миллисекундах)
+    });
+
