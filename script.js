c0b55fd3a9d079b9bcfd7015e150adc49d2bf278
Allmite <nurshat260@gmail.com>
2024-11-15 21:04:05 +0500

    add project

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-const sidebar = document.querySelector('.sidebar');
-const openBtn = document.querySelector('.open-btn');
-const closeBtn = document.querySelector('.close-btn');
 
-openBtn.addEventListener('click', () => {
-    sidebar.style.width = '250px';
-});
-
-closeBtn.addEventListener('click', () => {
-    sidebar.style.width = '0';
-});
